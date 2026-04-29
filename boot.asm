bd10ff4b1afa32cf76c96eb6f90bafa5d4881163
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2026-04-30 02:52:08 +0300

    Update boot.asm to define partition table entries and adjust sector fill size

--- boot.asm
+++ boot.asm
@@ -1,4 +1,3 @@
-ORG 0                   ; Place the origin of our address to 0x7c00
 BITS 16                 ; Switch to 16-bit code to be able to work in real mode
 
 jmp 0x7c0:start         ; Set cs to 0x7c0 and ip to start (0)
@@ -36,5 +35,17 @@ print_char:
 
 message db "Hello, world!", 0
 
-times 510 - ($-$$) db 0 ;Fill the rest of the sector with zeros until byte number 510
-dw 0xaa55               ;Place the signature at the last two bytes (511 & 512)
+times 446 - ($-$$) db 0 ; Fill the rest of the sector with zeros until byte 446
+
+; Partition Table Entry 1
+db 0x80                 ; Boot indicator, 0x80 means active/bootable (0b10000000), 0x00 means inactive
+db 0x00, 0x02, 0x00     ; Starting CHS (Dummy values), BIOS ignores it for type 0x0C
+db 0x0C                 ; Partition type, 0x0C = FAT32 with LBA
+db 0xFF, 0xFF, 0xFF     ; Ending CHS (Dummy values)
+dd 2048                 ; Starting LBA. Modern partitions typically start at sector 2048
+dd 125120120            ; Size in sectors: 125,120,120 sectors (60 GB) The size of our flash drive
+
+; Partition Table Entries 2, 3, and 4 (Unused)
+times 48 db 0           ; Fill the next 48 bytes with zeros (The last 3 entries)
+
+dw 0xaa55               ; Place the signature at the last two bytes (510 & 511)
