6a0f7f904e5b609bf13af87860eb9d6622c8489c
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2026-04-30 03:00:15 +0300

    Refactor comments and update partition table entry formatting in boot.asm

--- boot.asm
+++ boot.asm
@@ -4,13 +4,15 @@ jmp 0x7c0:start         ; Set cs to 0x7c0 and ip to start (0)
 
 start:
     cli                 ; Clear interrupts
+    ; Update registers
     mov ax, 0x7c0
-    mov ds, ax          ; Update the data segment register to 0x7c0
-    mov es, ax          ; Update the extra segment register to 0x7c0
+    mov ds, ax          ; Set the data segment register to 0x7c0
+    mov es, ax          ; Set the extra segment register to 0x7c0
     mov ax, 0x00        
-    mov ss, ax          ; Update the stack segment register to 0x00
-    mov sp, 0x7c00      ; Update the stack pointer to 0x7c00
-    sti                 ; Enable interrupts
+    mov ss, ax          ; Set the stack segment register to 0x00
+    mov sp, 0x7c00      ; Set the stack pointer to 0x7c00
+
+    sti                 ; Renable interrupts
 
     mov si, message     
     call print
@@ -22,7 +24,7 @@ print:
 .loop:
     lodsb               
     cmp al, 0
-    je .done            ; Return if string is done
+    je .done            ; Return if string is done (char == 0)
     call print_char     ; Else -> print the char
     jmp .loop
 .done:
@@ -38,11 +40,11 @@ message db "Hello, world!", 0
 times 446 - ($-$$) db 0 ; Fill the rest of the sector with zeros until byte 446
 
 ; Partition Table Entry 1
-db 0x80                 ; Boot indicator, 0x80 means active/bootable (0b10000000), 0x00 means inactive
-db 0x00, 0x02, 0x00     ; Starting CHS (Dummy values), BIOS ignores it for type 0x0C
+db 0x80                 ; Boot indicator, 0x80 means active/bootable (0b10000000), 0x00 means inactive/invalid
+times 3 db 0            ; Starting CHS (Dummy values), BIOS ignores it for type 0x0C
 db 0x0C                 ; Partition type, 0x0C = FAT32 with LBA
-db 0xFF, 0xFF, 0xFF     ; Ending CHS (Dummy values)
-dd 2048                 ; Starting LBA. Modern partitions typically start at sector 2048
+times 3 db 0            ; Ending CHS (Dummy values)
+dd 2048                 ; Starting LBA. Modern partitions typically start at sector 2048. You can totally make 1 (Start from the second sector)
 dd 125120120            ; Size in sectors: 125,120,120 sectors (60 GB) The size of our flash drive
 
 ; Partition Table Entries 2, 3, and 4 (Unused)
