d93cf47f1cb0e4dc3147ceb0599be170ed8cad7e
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2026-05-18 21:08:51 +0300

    Refactor terminal functions to remove video memory parameter; fix typos and improve clarity

--- src/kernel.c
+++ src/kernel.c
@@ -2,64 +2,69 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// Global pointer to the VGA video memory
+uint16_t* video_mem = NULL;
+
 uint16_t terminal_make_char(char c, char color){
     return (color << 8) | c;
 }
 
-void init_terminal(uint16_t** video_mem){
-    *video_mem = VGA_MEMORY;
+// Forward declaration
+void terminal_put_char(int x, int y, char c, char color);
+
+void init_terminal(void){
+    video_mem = VGA_MEMORY;
 
     // Clear the terminal
-    for(int y = 0;y<VGA_HEIGHT;++y){
-        for(int x = 0;x<VGA_WIDTH;++x){
-            terminal_put_char(x,y,' ',0,*video_mem);
+    for(int y = 0; y < VGA_HEIGHT; ++y){
+        for(int x = 0; x < VGA_WIDTH; ++x){
+            terminal_put_char(x, y, ' ', 0);
         }
     }
 }
 
-// Returns the lenght of a c-style string
+// Returns the length of a c-style string
 size_t strlen(const char* str){
     size_t length = 0;
     while(str[length]) length++;
     return length;
 }
 
-void terminal_write_char(char c, char color, size_t* col, size_t* row, uint16_t* video_mem){
+void terminal_write_char(char c, char color, size_t* col, size_t* row){
     if(c == '\n'){
-        // Move the cursor to the begging of the next line
+        // Move the cursor to the beginning of the next line
         (*row)++;
         *col = 0;
         return;
     }
-    terminal_put_char(*col, *row, c, color, video_mem);
+    terminal_put_char(*col, *row, c, color);
     (*col)++; // Move the cursor to the right one char
     if(*col >= VGA_WIDTH){ 
         // We reached the end of the line
-        // Move the cursor to the begging of the next line
+        // Move the cursor to the beginning of the next line
         *col = 0;
         (*row)++;
+        (*row) %= VGA_HEIGHT;
     }
 }
 
-void terminal_put_char(int x, int y, char c, char color, uint16_t* video_mem){
-    video_mem[(y*VGA_WIDTH) + x] = terminal_make_char(c, color);
+void terminal_put_char(int x, int y, char c, char color){
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
 }
 
-void print(const char* str, uint16_t* video_mem){
+void print(const char* str){
     static size_t col = 0;
     static size_t row = 0;
     size_t length = strlen(str);
-    for(int i = 0;i<length;++i){
+    for(int i = 0; i < length; ++i){
         // Print each character of the str to the console
-        terminal_write_char(str[i], WHITE, &col, &row, video_mem);
+        terminal_write_char(str[i], WHITE, &col, &row);
     }
 }
 
-void kernel_main(){
-    uint16_t* video_mem = NULL;
+void kernel_main(void){
+    init_terminal();
 
-    init_terminal(&video_mem);
-
-    print("Hello,world\n", video_mem);
-    print("Second line", video_mem);
+    print("Hello, world\n");
+    print("Second line");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,5 +1,6 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
+
 #include <stdint.h>
 #include <stddef.h>
 
@@ -21,19 +22,18 @@ enum color{
     LIGHT_GREEN,
     LIGHT_CYAN,
     LIGHT_RED,
-    LIGHT_MAGNETA,
+    LIGHT_MAGENTA,
     YELLOW,
     WHITE,
 };
 
-
-
+// Function Prototypes
 uint16_t terminal_make_char(char c, char color);
 size_t strlen              (const char* str);
-void terminal_put_char     (int x, int y, char c, char color, uint16_t* video_mem);
-void terminal_write_char   (char c, char color, size_t* col, size_t* row, uint16_t* video_mem);
-void print                 (const char* str, uint16_t* video_mem);
-void init_terminal         (uint16_t** video_mem);
-void kernel_main           ();
+void terminal_put_char     (int x, int y, char c, char color);
+void terminal_write_char   (char c, char color, size_t* col, size_t* row);
+void print                 (const char* str);
+void init_terminal         (void);
+void kernel_main           (void);
 
 #endif
